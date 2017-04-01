4fb4555d04c99c81f9239a37577ea42d1e047432
JoseMPena <josmpena91@gmail.com>
2017-04-01 18:11:00 +0200

    Add Product, database and a bit of styling

---
+++ db/seeds.rb
@@ -0,0 +1,14 @@
+unless Rails.env.production?
+  Product.delete_all
+  Product.create(
+    title: 'Seed product', 
+    description:
+      %{<p>
+        Ruby is the fastest growing and most exciting dynamic language
+        out there. If you need to get working programs delivered fast,
+        you should add Ruby to your toolbox.
+      </p>},
+    image_url: 'ruby.jpg',
+    price: 49.95
+  )
+end
