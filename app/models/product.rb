6ce14fec2e25a06bfa480635c350937cedd77dfc
JoseMPena <josmpena91@gmail.com>
2017-04-12 09:59:35 +0200

    Validations - GREEN

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,10 @@
 class Product < ApplicationRecord
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
-  validates :title, uniqueness: true
+  validates :title, uniqueness: true, length: {
+    minimum: 10,
+    too_short: 'should have at least 10 characters'
+  }
   validates :image_url, allow_blank: true, format: {
     with: /\.(gif|jpg|png)\Z/i,
     message: 'must be GIF, JPG or PNG.'
