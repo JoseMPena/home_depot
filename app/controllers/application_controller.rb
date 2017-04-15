39aee9c9974f524e4cfa695bcd3fa6e5cac47092
JoseMPena <josmpena91@gmail.com>
2017-04-15 22:05:05 +0200

    Add shopping cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+
+  def current_cart
+    cart = Cart.where(id: session[:cart_id]).first_or_create
+    session[:cart_id] = cart.id
+    cart
+  end
 end
