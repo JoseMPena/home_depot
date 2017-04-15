567af9a24670dee2d35cd4babd8f4167f64b72e5
JoseMPena <josmpena91@gmail.com>
2017-04-15 21:55:21 +0200

    Test index controller and view (sort of) - GREEN

--- spec/views/store/index.html.erb_spec.rb
+++ spec/views/store/index.html.erb_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
+require 'capybara/rspec'
 
-RSpec.describe "store/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'store/index.html.erb', type: :view do
+  before :all do
+    @products = Product.all
+  end
+  it 'renders the nav menu' do
+    render
+    rendered.should have_selector('.entry', count: @products.count)
+    assert_select 'h3', @products.first.title
+    assert_select '.price', /\$[,\d]+\.\d\d/
+  end
 end
