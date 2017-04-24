1646cbb026d9111f1675528a02e88ee5b420e0ec
JoseMPena <josmpena91@gmail.com>
2017-04-24 21:13:53 +0200

    Line Item controller - GREEN

--- spec/controllers/line_items_controller_spec.rb
+++ spec/controllers/line_items_controller_spec.rb
@@ -1,9 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe LineItemsController, type: :controller do
+  let(:valid_attributes) {
+    skip('Add a hash of attributes valid for your model')
+  }
 
+  let(:invalid_attributes) {
+    skip('Add a hash of attributes invalid for your model')
+  }
+  
+  let(:valid_session) { {} }
+  
   describe "GET #index" do
-    it "assigns all line_items as @line_items" do
+    it 'assigns all line_items as @line_items' do
       line_item = LineItem.create! valid_attributes
       get :index, params: {}, session: valid_session
       expect(assigns(:line_items)).to eq([line_item])
