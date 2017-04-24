2047f23f8b009c46b43e06b24819ef137ae8a179
JoseMPena <josmpena91@gmail.com>
2017-04-24 20:38:34 +0200

    Add error handling on show

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,13 @@
 class CartsController < ApplicationController
   before_action :set_cart, only: [:show, :edit, :update, :destroy]
-  
+
+  def show
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @cart }
+    end
+  end
+
   def index
     @carts = Cart.all
   end
@@ -47,7 +54,12 @@ class CartsController < ApplicationController
 
   # Use callbacks to share common setup or constraints between actions.
   def set_cart
-    @cart = Cart.find(params[:id])
+    begin
+      @cart = Cart.find params[:id]
+    rescue ActiveRecord::RecordNotFound
+      logger.error "Attempt to access invalid cart #{params[:id]}"
+      redirect_to store_url, notice: 'Invalid cart'
+    end
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
