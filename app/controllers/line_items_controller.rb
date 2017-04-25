0d1a1f53af4771db2fa91e0279aa9fde85f98c54
JoseMPena <josmpena91@gmail.com>
2017-04-25 09:18:19 +0200

    Hide empty carts

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -17,7 +17,7 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to store_url }
-        format.js
+        format.js { @current_item = @line_item }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
@@ -41,7 +41,7 @@ class LineItemsController < ApplicationController
   def destroy
     @line_item.destroy
     respond_to do |format|
-      format.html { redirect_to line_items_url, notice: 'Line item was successfully destroyed.' }
+      format.html { redirect_to line_items_url }
       format.json { head :no_content }
     end
   end
