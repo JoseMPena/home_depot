131e9e21d290ded2193f9e287c793a8adc0d5576
JoseMPena <josmpena91@gmail.com>
2017-04-26 10:07:40 +0200

    Make images clickable

--- spec/controllers/line_items_controller_spec.rb
+++ spec/controllers/line_items_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe LineItemsController, type: :controller do
+  fixtures :products
   let(:valid_attributes) {
     skip('Add a hash of attributes valid for your model')
   }
@@ -18,5 +19,19 @@ RSpec.describe LineItemsController, type: :controller do
       expect(assigns(:line_items)).to eq([line_item])
     end
   end
+  
+  describe 'POST #create' do
+    render_views
+    it 'creates line item' do
+      expect { post :create, product_id: products(:ruby).id }
+        .to change { LineItem.count }.by(1)
+      expect(response).to redirect_to store_path
+    end
 
+    it 'creates a line_item via ajax' do
+      expect { xhr :post, :create, product_id: products(:ruby).id }
+        .to change { LineItem.count }.by(1)
+      expect(response).to have_http_status :success
+    end
+  end
 end
--- spec/controllers/store_controller_spec.rb
+++ spec/controllers/store_controller_spec.rb
@@ -2,10 +2,17 @@ require 'rails_helper'
 
 RSpec.describe StoreController, type: :controller do
 
-  describe "GET #index" do
-    it "returns http success" do
+  describe 'GET #index' do
+    render_views
+    it 'returns http success' do
       get :index
       expect(response).to have_http_status(:success)
+    end
+
+    it 'has store.js.coffee is in place' do
+      get :index
+      assert_select '.store .entry > img', 3
+      assert_select '.entry input[type=submit]', 3
     end
   end
 
